07c77d71664b3204d40e6fc918f619809744e447
watcharawit <watcharawit.saejung@gmail.com>
2025-09-24 16:04:12 +0700

    update packet

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+require('dotenv').config()
+
 const app = express()
-const { PORT } = process.env
+const PORT = process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello, Express from server!')
