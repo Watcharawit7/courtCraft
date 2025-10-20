eca8f7ec8baff559ecc5ecd1a288fcc7ce9c2912
Watcharawit7 <watcharawit.saejung@gmail.com>
2025-10-21 00:06:46 +0700

    [Check] git

--- client/src/components/pages/Layout/Layout.jsx
+++ client/src/components/pages/Layout/Layout.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Login from '../LoginForm/Login'
 import Register from '../LoginForm/Register'
+import Booking from '../Booking/Booking'
 
 const Layout = () => {
   return (
@@ -10,6 +11,7 @@ const Layout = () => {
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/booking" element={<Booking />} />
       </Routes>
     </div>
   )
